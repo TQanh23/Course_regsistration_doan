61a56f9b943d9b23e2ef64dd32e4efe1eba8c8f9
Kuonng <144304225+NHCuong177@users.noreply.github.com>
2025-04-08 09:41:24 +0700

    Update LoadingScreen and LoginScreen

--- dkhpmobile/screens/LoadingScreen.tsx
+++ dkhpmobile/screens/LoadingScreen.tsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Modal } from "react-native";
 import LottieView from "lottie-react-native";
 
 const LoadingScreen: React.FC = () => {
   return (
-    <View style={styles.container}>
+    <Modal transparent animationType="fade">
+      <View style={styles.container}>
         <View style={styles.loading}>
-          <LottieView style={{flex: 1}} source={require("../assets/loading.json")} autoPlay loop={true} />
+          <LottieView 
+            style={{flex: 1}} 
+            source={require("../assets/loading.json")} 
+            autoPlay 
+            loop={true} 
+          />
         </View>
-      
-    </View>
+      </View>
+    </Modal>
   );
 };
 
@@ -20,10 +26,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
   loading: {
     height: 300,
-    aspectRatio: 1
+    aspectRatio: 1,
+    backgroundColor: "#fff",
+    borderRadius: 16,
+    padding: 20,
   }
 });
