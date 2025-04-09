ca4f9b173271f06980ccfd03956899a083ee5139
strawberrymilktea0604 <4004267@st.huce.edu.vn>
2025-04-09 11:44:18 +0700

    add code vào dkhpmobile

--- dkhpmobile/app/index.tsx
+++ dkhpmobile/app/index.tsx
@@ -1,41 +1,17 @@
-import React, { useState, useEffect, useRef } from "react";
-import { View, StyleSheet, Animated } from "react-native";
-import LoadingScreen from "../screens/LoadingScreen";
-import LoginScreen from "../screens/LoginScreen";
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Navigation from '../navigation/Navigation';
 
-const App: React.FC = () => {
-  const [currentScreen, setCurrentScreen] = useState<"LoadingScreen" | "LoginScreen">("LoadingScreen");
-  const fadeAnim = useRef(new Animated.Value(1)).current; // Bắt đầu với opacity 1 (hiển thị LoadingScreen)
-
-  useEffect(() => {
-    setTimeout(() => {
-      // Fade out LoadingScreen
-      Animated.timing(fadeAnim, {
-        toValue: 0,
-        duration: 1000,
-        useNativeDriver: true,
-      }).start(() => {
-        setCurrentScreen("LoginScreen"); // Chuyển màn hình sau khi fade out
-        Animated.timing(fadeAnim, {
-          toValue: 1,
-          duration: 1000,
-          useNativeDriver: true,
-        }).start(); // Fade in LoginScreen
-      });
-    }, 3000);
-  }, []);
-
+const App = () => {
   return (
-    <Animated.View style={[styles.container, { opacity: fadeAnim }]}>
-      {currentScreen === "LoadingScreen" ? <LoadingScreen /> : <LoginScreen />}
-    </Animated.View>
+    <SafeAreaProvider>
+      <Navigation />
+    </SafeAreaProvider>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+const appName = 'thunghiem';
+AppRegistry.registerComponent(appName, () => App);
