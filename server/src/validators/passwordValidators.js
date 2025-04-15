5996d6b59eabda71eeb6e7f6c8c7bab3a2c3f781
Kuonng <144304225+NHCuong177@users.noreply.github.com>
2025-04-15 07:29:24 +0700

    Create passwordValidators.js

---
+++ server/src/validators/passwordValidators.js
@@ -0,0 +1,6 @@
+function isStrongPassword(password) {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/;
+    return regex.test(password);
+  }
+  
+  export default { isStrongPassword };
